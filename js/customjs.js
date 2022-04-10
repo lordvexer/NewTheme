d3198e4432b223cc59683e892b9e2642449eda59
HamidReza Mahmoudzadeh <h.nypd@yahoo.com>
2022-04-10 14:36:39 +0430

    modal fix

--- js/customjs.js
+++ js/customjs.js
@@ -3,14 +3,6 @@ function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
     $('#' + cityName).parent().find('.tabcontent').removeClass('active').css('display', 'none');
     $('#' + cityName).parent().find('.tablinks').removeClass('active');
-    // tabcontent = document.getElementsByClassName("tabcontent");
-    // for (i = 0; i < tabcontent.length; i++) {
-    //     tabcontent[i].style.display = "none";
-    // }
-    // tablinks = document.getElementsByClassName("tablinks");
-    // for (i = 0; i < tablinks.length; i++) {
-    //     tablinks[i].className = tablinks[i].className.replace(" active", "");
-    // }
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
